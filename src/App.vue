5fd5a97de2c1137f0614eb63544da60c877cf63f
lizongxiao <158638447@qq.com>
2025-02-13 11:29:34 +0800

    msg

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld msg="这是一个在线的vercel测试" />
 </template>
 
 <script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: "这是一个在线的vercel测试",
+  name: "HelloWorld",
   props: {
     msg: String,
   },
